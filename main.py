d3fbeb1eb7a749d164daa449fb70cf586219f0a3
BlackAsu <44605334+Z01Ntasu@users.noreply.github.com>
2018-12-28 17:11:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os 
-
 try:
 	from win32gui import PumpMessages
 except:
-	print ("Trying to Install required module: pypi\n")
+	print ("Installing pypi\n")
 	os.system('python -m pip install pypiwin32')
 try:
 	from pyHook import HookManager
 except:
-	print ("Trying to Install required module: pyHook\n")
+	print ("Installing pyHook\n")
 	os.system('python -m pip install pyHook-1.5.1-cp37-cp37m-win_amd64.whl')
-	
 
 print("Press 4 on the Keyboard to activate and deactivate the Laggswitch")
 
@@ -21,57 +19,23 @@ class Keystroke_Watcher(object):
 		self.hm.HookKeyboard()
 		self.alreadyOn = False
 
-
 	def on_keyboard_event(self, event):
 		try:
 			if event.KeyID  == 52:
-				self.your_method()
+				self.switchRule()
 		finally:
 			return True
 
-	def your_method(self):
+	def switchRule(self):
 		if (self.alreadyOn == False):
 			self.alreadyOn = True
-			
-			string = 'netsh advfirewall firewall set rule name="Arma3Laggswitch" new enable=' + "yes"
-			os.system(str(string))
-			print("active")
-		else:
-			
-			self.alreadyOn = False
-			string = 'netsh advfirewall firewall set rule name="Arma3Laggswitch" new enable=' + "no"
+			string = 'netsh advfirewall firewall set rule name="Arma 3" new enable=' + "yes"
 			os.system(str(string))
 			print("not active")
-			
-	
-        
-
-	def shutdown(self):
-		PostQuitMessage(0)
-		self.hm.UnhookKeyboard()
-
-
-with open("config.txt", "r+") as file:
-	data = file.readlines()
-
-if data[0] == "false":
-	
-	
-	gamePath = input("Enter the path of Arma\n")
-		
-	command = 'netsh advfirewall firewall add rule name="Arma3Laggswitch" dir=out action=block program="' + gamePath + '" enable=no'
-	os.system(command)
-	print("Regel erstellt")
-	data[0] = "true"
-	open("config.txt",'w').write('\n'.join(data))
-
-
+		else:
+			self.alreadyOn = False
+			string = 'netsh advfirewall firewall set rule name="Arma 3" new enable=' + "no"
+			os.system(str(string))
+			print("active")
 watcher = Keystroke_Watcher()
 PumpMessages()
-
-
-
-
-
-
-
